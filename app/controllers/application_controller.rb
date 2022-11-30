064040ac621449b49627a699025ece2e0e858111
jacksongoldie <goldiejackson25@yahoo.com>
2022-11-30 13:48:00 -0600

    user and contact db setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    respond_to :json
+    
+    private
+  
+    def record_invalid(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def record_not_found
+      render json: { errors: 'Record not found' }, status: :not_found
+    end
+
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
