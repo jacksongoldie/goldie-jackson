4a1df41bccbf0268b082f56d937097b95307da5b
jacksongoldie <goldiejackson25@yahoo.com>
2022-12-01 11:31:27 -0600

    change card size

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -14,7 +14,7 @@ function ProjectCard({ project }) {
 
   return (
     <div>
-      <MDBRow className='row-cols-1 row-cols-md-2 g-4'>
+      <MDBRow className='justify-content-center'>
       <MDBCol>
       <MDBCard className='mb-3' style={{ margin: '5%'}}>
         {project.image ? <MDBCardImage position='top' src={project.image} alt='...' /> : null }
@@ -23,11 +23,10 @@ function ProjectCard({ project }) {
           <MDBCardText>
             {project.description}
           </MDBCardText>
-          <MDBBtnGroup>
           {project.deploy_url ? <MDBBtn style={{margin:'1em', backgroundColor: 'pink', color:'black'}} tag='a' href={project.deploy_url} title='View App' target='_blank' >Live</MDBBtn> : null}
           {project.video_url ? <MDBBtn style={{margin:'1em', backgroundColor: 'pink', color:'black'}} tag='a' href={project.video_url} title='Watch Demo' target='_blank' >Video</MDBBtn> : null}
           {project.github_url ? <MDBBtn style={{margin:'1em', backgroundColor: 'pink', color:'black'}} tag='a' href={project.github_url} title='View Code' target='_blank' >Code</MDBBtn> : null}
-          </MDBBtnGroup>
+          
         </MDBCardBody>
       </MDBCard>
       </MDBCol>
